96ccd04e43a0db872cffc09230c2005371a110dd
Andrey <9400878@gmail.com>
2019-09-04 15:18:22 +0300

    update check new files

--- check_new_file.py
+++ check_new_file.py
@@ -13,17 +13,20 @@ link = 'ftp://ftp.cmegroup.com/pub/span/data/cme/'
 today = date.today()
 
 # mounths = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
-pa2_files = []
+
 
 
 def check():
+    new_cme = []
     cme_data = {}
+    pa2_files = []
 
-    # with open('cme_data.json', "r") as cme_data:
-    #     try:
-    #         cme_data = json.load(cme_data)
-    #     except json.decoder.JSONDecodeError:
-    #         print('Ошибка открытия файла')
+    with open('cme_data.json', "r") as cme_data:
+        try:
+            cme_data = json.load(cme_data)
+        except json.decoder.JSONDecodeError:
+            cme_data = {}
+            # print('Ошибка открытия файла')
 
 
         # newses = newses[::-1]
@@ -39,29 +42,38 @@ def check():
 
         for i in range(len(listing)):
             words = listing[i].split(None, 8)
+            # print('words', words)
+
             filename = words[-1].lstrip()
+            # print(filename)
             if '.pa2.zip' in filename:
+                # print(filename)
                 pa2_files.append(filename)
+
                 # print(datetime.date(year='', month=words[-4], day=words[-3]))
                 # print('words', words)
                 # t4 = datetime(year=today.year, month=mounths[words[-4]], day=words[-3], hour=words[-2].split(':')[0], minute=words[-2].split(':')[1], second=0)
 
                 # print(words, filename)
-    # for file in pa2_files:
-    #     file_add = file.split('.')
-    #     print(file_add)
-    #
-    #     if file_add[2] in cme_data:
-    #         if file_add[1] not in cme_data[file_add[2]]:
-    #             cme_data[file_add[2]].append(file_add[1])
-    #     else:
-    #         cme_data[file_add[2]] = []
-    #         cme_data[file_add[2]].append(file_add[1])
-    #
-    #
-    # json.dump(cme_data, open("cme_data.json", "w"))
+    for file in pa2_files:
+        file_add = file.split('.')
+        # print(file_add[2])
+
+        if len(file_add) == 5 and file_add[2] in cme_data:
+            if file_add[1] not in cme_data[file_add[2]]:
+                cme_data[file_add[2]].append(file_add[1])
+                new_cme.append(file)
+        elif len(file_add) == 5:
+            # print(file_add[2])
+            # cme_data[file_add[2]] = []
+            cme_data[file_add[2]] = [file_add[1]]
+            new_cme.append(file)
+
+    json.dump(cme_data, open("cme_data.json", "w"))
+
+    return new_cme
 
 
-check()
+pa2_files = check()
 print(len(pa2_files))
-print(pa2_files)
+# print(pa2_files)
